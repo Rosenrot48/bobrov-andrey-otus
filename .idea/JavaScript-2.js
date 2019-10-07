36a6191162fc79e9350a1f1ccd20f5615a1b3573
andrey <Herzeleid48>
2019-10-07 11:53:53 +0300

    другая реализация

--- .idea/JavaScript-2.js
+++ .idea/JavaScript-2.js
@@ -5,19 +5,20 @@ async function promiseReduce( asyncFunctions, reduce, initialValue) {
   var initialInputData = Promise.resolve(initialValue);
   asyncFunctions.forEach( async promise =>
     {
-       const promiseResult = await promise();
-      initialValue = reduce(initialValue, promiseResult);
-      // console.log(initialValue);
+      console.log(await promise());
+       // const promiseResult = await promise();
+      initialValue = reduce(initialValue, await promise());
+      console.log(initialValue);
       return initialValue;
     })
 }
 
 
-const func1 = () => Promise.resolve(1);
+const func1 = () => Promise.resolve(2);
 const func2 = () => new Promise(resolve => {
-  setTimeout(() => resolve(2), 1000);
+  setTimeout(() => resolve(4), 1000);
 });
 
-promiseReduce([func1,func2], (memo, value) => memo * value, 2);
+promiseReduce([func1,func2], (memo, value) => memo * value, 88);
 
 
