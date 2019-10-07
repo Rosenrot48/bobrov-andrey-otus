ba87a9b01be24369697ebd6ea8f57054082fca5c
andrey <Herzeleid48>
2019-10-07 11:50:01 +0300

    переосмысление задачи

--- .idea/JavaScript-2.js
+++ .idea/JavaScript-2.js
@@ -1,20 +1,23 @@
-async function promiseReduce(asyncFunctions, reduce, initialValue) {
-  var initialdata = Promise.resolve(1);
-  asyncFunctions.forEach(
-    response => {
-      initialdata = initialdata.then(
-        async firstData => {
-          return reduce(firstData, await response()) * initialValue;
-        })
+// 1) нужна промежуточная переменная, которая будет хранить данные для перемножения
+// 2) сделать подписку на promise, ждать получения данных, передевать их в функцию reduce вторым аргументом
+
+async function promiseReduce( asyncFunctions, reduce, initialValue) {
+  var initialInputData = Promise.resolve(initialValue);
+  asyncFunctions.forEach( async promise =>
+    {
+       const promiseResult = await promise();
+      initialValue = reduce(initialValue, promiseResult);
+      // console.log(initialValue);
+      return initialValue;
     })
 }
 
 
-const func1 = () => Promise.resolve(2);
+const func1 = () => Promise.resolve(1);
 const func2 = () => new Promise(resolve => {
-  setTimeout(() => resolve(4), 1000);
+  setTimeout(() => resolve(2), 1000);
 });
 
-promiseReduce([func1,func2], (memo, value) => memo * value, 88);
+promiseReduce([func1,func2], (memo, value) => memo * value, 2);
 
 
