fddf250b06ee1a5774d277010deceaceced895d6
andrey <abobrov@reunico.com>
2019-10-27 21:36:51 +0300

    уникальный путь для элемента документа

---
+++ .idea/javascript-3.js
@@ -0,0 +1,26 @@
+
+    let pathArray = [];
+
+    var uniqPath;
+    function getPath(element) {
+        var path = element.nodeName.toLowerCase();
+        for (let i = 0; i < element.classList.length; i++) {
+            path = path + "." + element.classList[i]
+        }
+        pathArray.push(path);
+        if (element.parentElement) {
+            return getPath(element.parentElement)
+        } else {
+            // return pathArray
+            for (let c = pathArray.length - 1; c > 0; c--) {
+                if (c === pathArray.length - 1) {
+                    uniqPath = pathArray[c];
+                } else {
+                    uniqPath = uniqPath + ">" + pathArray[c];
+                }
+            }
+            // console.log(uniqPath);
+            return uniqPath;
+        }
+    }
+    document.querySelectorAll(uniqPath);
