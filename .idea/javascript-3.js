2fcc2e64bdc0d35adbaa2a31259c8701373f4f29
andrey <abobrov@reunico.com>
2019-10-31 11:52:03 +0300

    построение дерева и его детей

--- .idea/javascript-3.js
+++ .idea/javascript-3.js
@@ -1,62 +1,20 @@
-
+    function getPath(element) {
     let pathArray = [];
 
-    var uniqPath;
-    function getPath(element) {
-        var path = element.nodeName.toLowerCase();
-        // console.log(element.attributes);
-        for (let i = 0; i < element.classList.length; i++) {
-            path = path + "." + element.classList[i]
-        }
-        pathArray.push(path);
-        if (element.parentElement) {
-            return getPath(element.parentElement)
-        } else {
-           return pathArray
-            for (let c = pathArray.length - 1; c > 0; c--) {
-                if (c === pathArray.length - 1) {
-                    uniqPath = pathArray[c];
-                } else {
-                    uniqPath = uniqPath + ">" + pathArray[c];
-                }
-            }
-            // console.log(uniqPath);
-            path = null;
-            pathArray = [];
-            // return document.querySelectorAll(uniqPath);
-            return uniqPath;
-        }
-
-
+    function getNth(element) {
+        return  `${element.nodeName}:nth-child(${Array.from(element.parentNode.children).indexOf(element)+1})`;
     }
-    document.querySelectorAll(getPath($0));
-
-
-
-
-
-
-
-
-
-
-
-    let pathArray = [];
-
-    var uniqPath;
-
-    function getPath(element) {
-        if (element.childElementCount > 1) {
-            for (let t of element.childNodes) {
-                if (t === element) {
-                    console.log(t);
-                    console.log('equals');
-                    console.log(element)
-                }
-            }
+    function getId(element) {
+        if (element.id){
+            return `#${element.id}`
         }
-        if (element.parentElement) {
-            console.log(element.parentElement);
-            return getPath(element.parentElement);
-        }
+        pathArray.push(getNth(element));
+        return getId(element.parentElement);
     }
+    let uniqPath = getId(element);
+    while(pathArray.length > 0) {
+        uniqPath += `>${pathArray.pop()}`
+    }
+    return uniqPath;
+    }
+    document.querySelectorAll(getPath($0))
