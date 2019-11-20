60eb0c4d58be5995d0769a6a7f1e9fb07e27ec73
andrey <abobrov@reunico.com>
2019-11-21 01:17:22 +0300

    передача данных через консоль

---
+++ javascript/javascript-5/request.js
@@ -0,0 +1,41 @@
+const url = 'http://127.0.0.1:7000';
+const readline = require('readline');
+let requestsCount = 0;
+let isAsync = null;
+async function requestToServer(requestsCount, isAsync) {
+    for (let i =1; i < requestsCount+1; i++) {
+        if (isAsync === false) {
+            await request();
+        } else {
+            request();
+        }
+    }
+}
+
+function request() {
+    return new Promise((resolve) => {
+        require('javascript/javascript-5/request')(url, function (error, response, body) {
+            console.log(body);
+            resolve()
+        });
+    })
+}
+const read =  readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+// requestToServer(5,false);
+read.question(`Укажите количество запросов на сервер `, (answer) => {
+    read.question(`Укажите тип запросов (асинхронные или синхронные) `, answer1 => {
+        requestsCount = answer;
+        if (answer1 === 'асинхронные') {
+            isAsync = true;
+        } else if (answer1 === 'синхронные') {
+            isAsync = false;
+        } else {
+            isAsync = answer1;
+        }
+        requestToServer(requestsCount, isAsync);
+        read.close();
+    });
+});
